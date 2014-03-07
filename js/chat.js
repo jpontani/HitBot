5b536b7b82ef3fb327a6aa4d84c2f0f59e69f93a
Joseph Pontani <jpontani@vt.edu>
2014-03-07 15:28:49 -0500

    Split DB/Bot objects. Starting Queue object for command processing. Some dashboard work.

--- js/chat.js
+++ js/chat.js
@@ -1,89 +1,120 @@
 /**
- * Created by jpontani on 2/25/14.
+ * Created by TheMoose on 2/25/14.
  */
 
+var version = 0.1;
+var chatMessages = $("section.chatContent ul");
+var li = 0;
+var SELF = "MooseBot";
 
-var CREATE = "CREATE TABLE IF NOT EXISTS ";
-var TABLES = ["users(uid INTEGER PRIMARY KEY ASC, username VARCHAR(200), points INTEGER, level INTEGER)",
-              "commands(cid INTEGER PRIMARY KEY ASC, command VARCHAR(50), response TEXT, level INTEGER)",
-              ""];
-var LootBot = function () {
-    this.errors = [];
-    this.data.db = null;
+// @TODO: Create queue object.
 
-    this.data.open = function() {
-        // 5MB of space should be plenty for basic text/numeric data.
-        var dbSize = 5 * 1024 * 1024;
-        this.db = openDatabase("LootBot", 1, "LootBot Data", dbSize);
-    }
+var Queue = (function(){
+    var q = [];
+    var size = 0;
+    return {
+        enqueue: function(item) {
+            // Add the item to the end of the queue.
+            q[size++] = item;
+        },
+        dequeue: function(){
+            //@TODO: return/remove next entry
+        },
+        peek: function() {
+            // Simply peek at the next (front) item.
+            return q[0];
+        },
+        length: (function() {
+            return size;
+        })()
+    };
+})();
 
-    this.data.setupTables = function() {
-        var db = this.data.db;
-        TABLES.forEach(function(table) {
-            db.transaction(function(t){
-                t.executeSql(CREATE + table);
-            });
-        });
-    }
+var MooseBot = (function () {
+    var botData = MooseDB;
+    var commandQueue = [];
+    var plugins = [];
+    return {
+        bot: (function(){
+            return {
+                errors: {},
+                init: function() {
+                    botData.open();
+                },
+                commandRespond: function(command) {
+                    if (command.response == "") {
+                        return;
+                    }
+                    var chat = $("#chatInput");
+                    chat.html(command.response);
+                    var e = jQuery.Event("keypress");
+                    e.which = 13;
+                    e.keyCode = 13;
+                    chat.trigger(e);
+                },
+                handleCustomCommand: function(text, level, sender) {
+                    var command = botData.getCommand(text, level, sender);
+                    if (command != null) {
+                        this.commandRespond(command);
+                    }
+                },
+                isCustomCommand: function(cmd) {
 
-    this.data.onError = function (tx, e) {
-    }
-
-    this.data.onSuccess = function (tx, r) {
-        // Re-render all available commands and users.
-    }
-
-    this.data.addCustomCommand = function (command, response, level) {
-        // Add a custom text command
-    }
-
-    this.data.editCustomCommand = function (command, response, level) {
-        // edit an existing custom text command
-    }
-
-    this.data.removeCustomCommand = function (command) {
-
-    }
-
-    this.init = function() {
-        this.data.open();
-        this.data.setupTables();
-    }
-
-    this.commandRespond = function(tx, rs) {
-        if (rs.rows.length == 1) {
-            var response = "LootBot: " + rs.rows.item(0).response;
-            $("#chatInput").html(response);
-            var e = jQuery.Event("keypress");
-            e.which = 13;
-            e.keyCode = 13;
-            $("#chatInput").trigger(e);
-        }
-    }
-
-    this.handleCustomCommand = function(command, level) {
-        var db = this.data.db;
-        var bot = this;
-        db.transaction(function(tx) {
-            tx.executeSql("SELECT * FROM commands WHERE command=? AND level >= ?", [command, level], bot.commandRespond, bot.data.onError);
-        });
-    }
-
-    this.handleChat = function(newMessageItem) {
-        var sender = newMessageItem.find("span.name").html();
-        var message = newMessageItem.find("span.message").html();
-        var level = 2;
-        if (newMessageItem.hasClass("admin")) {
-            level = 0;
-        }
-        else if (newMessageItem.hasClass("mod")) {
-            level = 1;
-        }
-        var parse = message.split(" ");
-        if (parse.substr(0, 1) == "!") {
-            if (parse.length == 1) {
-                this.handleCustomCommand(parse[0], level);
+                },
+                doCommand: function(text, level, sender) {
+                    var cmd = botData.getCommand(text);
+                },
+                handleChat: function(newMessageItem) {
+                    var sender = newMessageItem.children[0].children[1].innerHTML;
+                    var message = newMessageItem.children[0].children[2].innerHTML;
+                    alert(message);
+                    var level = 2;
+                    if ($(newMessageItem).hasClass("admin")) {
+                        level = 0;
+                    }
+                    else if ($(newMessageItem).hasClass("mod")) {
+                        level = 1;
+                    }
+                    var parse = message.split(" ");
+                    if (parse.substr(0, 1) == "!") {
+                        if (parse.length == 1) {
+                            // @TODO: Add command to queue.
+                            this.bot.doCommand(parse[0], level, sender);
+                        }
+                    }
+                }
+            };
+        })(),
+        listen: function() {
+            var items = $("section.chatContent ul li");
+            if (items.length > li) {
+                // Chat commands updated
+                var bot = this.bot;
+                alert(li + ":" + items.length);
+                for (var i=li; i<items.length; i++) {
+                    bot.handleChat(items[i]);
+                }
+                li = items.length;
             }
+        },
+        start: function() {
+            this.bot.init();
+            this.listen();
+            botData.log("MooseBot version " + version + " initialized. Now accepting commands.");
         }
+    };
+})();
+
+var findConnected = setInterval("findConnectedMessage()", 1000);
+
+function findConnectedMessage() {
+    // Start the bot once the chat has fully loaded. This way we ignore any previous commands that were entered prior to
+    // the bot being available.
+    var items = $("div.bufferTimestamp div.ng-binding");
+    if (items.last().css("display") == "block") {
+        clearInterval(findConnected);
+        li = $("section.chatContent ul li").length;
+        MooseBot.start();
+        setInterval("MooseBot.listen()", 1000);
     }
-};
+}
--- js/database.js
+++ js/database.js
@@ -1,4 +1,83 @@
 /**
- * Created by jpontani on 2/25/14.
+ * Created by TheMoose on 2/25/14.
  */
 
+// In the following line, you should include the prefixes of implementations you want to test.
+window.indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB;
+// DON'T use "var indexedDB = ..." if you're not in a function.
+// Moreover, you may need references to some window.IDB* objects:
+window.IDBTransaction = window.IDBTransaction || window.webkitIDBTransaction || window.msIDBTransaction;
+window.IDBKeyRange = window.IDBKeyRange || window.webkitIDBKeyRange || window.msIDBKeyRange;
+// (Mozilla has never prefixed these objects, so we don't need window.mozIDB*)
+
+if (!window.indexedDB) {
+    window.alert("Your browser doesn't support a stable version of IndexedDB. Such and such feature will not be available.");
+}
+
+var MooseDB = (function() {
+    var mdb = null;
+    return {
+        db: (function(){
+            return mdb;
+        })(),
+        log: function(msg, appendToElem) {
+            $("#" + appendToElem).append("<div class='log-message'>" + msg + "</div>");
+        },
+        open: function() {
+            var version = 1;
+            var request = window.indexedDB.open("HitBot",version);
+            request.onsuccess = function(e) {
+                mdb = e.target.result;
+            };
+            request.onerror = function(e) {};
+            request.onupgradeneeded = function(e) {
+                this.log("The database store has been updated.");
+            };
+        },
+        setupStores: function() {
+            if (!mdb.objectStoreNames.contains("users")) {
+                var userStore = mdb.createObjectStore("users", {keyPath: "username"})
+                var commandStore = mdb.createObjectStore("commands", {keyPath: "command"});
+            }
+        },
+        addCustomCommand: function (command, response, level) {
+            // Add a custom text command
+            var tx = mdb.transaction(["commands"], "readwrite");
+            var store = tx.objectStore("commands");
+            var request = store.put({
+                "command": command,
+                "response": response,
+                "level": level
+            });
+            request.onsuccess = function(e) {
+                this.log("Command '" + command + "' added successfully.", 'dashboardLog');
+                return true;
+            }
+            request.onerror = function(e) {
+                // @TODO: put an error message in the log on the dashboard
+                this.log("Could not add command '" + command + "'.", 'dashboardLog');
+                return false;
+            }
+        },
+        editCustomCommand: function (command, response, level) {
+            // edit an existing custom text command
+        },
+        removeCustomCommand: function (command) {
+
+        },
+        getCommandList: function() {},
+        getCommand: function(command) {
+            var tx = mdb.transaction(["commands"], "readwrite");
+            var store = tx.objectStore("commands");
+            var key = IDBKeyRange.only(command);
+            var request = store.openCursor(key);
+            request.onsuccess = function(e) {
+                var result = e.target.result;
+                if (!!result == false) {
+                    return null;
+                }
+                return result;
+            };
+        }
+    };
+})();
